f676ca4d6b9976d7fcc0695b72345b3e67bb7ef6
DaniVV02 <122785639+DaniVV02@users.noreply.github.com>
2024-03-25 09:56:44 +0100

    Ajout testjs
    Pour tester si on recup bien la bdd

---
+++ test.js
@@ -0,0 +1,37 @@
+// Importer le module MySQL
+const mysql = require('mysql2');
+
+// Configuration de la connexion à la base de données
+const connection = mysql.createConnection({
+  host: 'localhost', // L'hôte de la base de données (par exemple, 'localhost' si la base de données est en local)
+  user: 'root', // Le nom d'utilisateur de la base de données
+  password: 'hai501', // Le mot de passe de la base de données
+  database: 'stock' // Le nom de la base de données à laquelle se connecter
+});
+
+// Établir la connexion à la base de données
+connection.connect((err) => {
+  if (err) {
+    console.error('Erreur de connexion à la base de données :', err.stack);
+    return;
+  }
+
+  console.log('Connecté à la base de données MySQL avec l\'identifiant', connection.threadId);
+});
+
+// Exécuter une requête SQL
+connection.query('SELECT * FROM ADRESSE', (err, rows) => {
+  if (err) throw err;
+
+  console.log('Données récupérées de la base de données :', rows);
+});
+
+// Fermer la connexion à la base de données après avoir exécuté la requête
+connection.end((err) => {
+  if (err) {
+    console.error('Erreur lors de la fermeture de la connexion à la base de données :', err.stack);
+    return;
+  }
+
+  console.log('Connexion à la base de données fermée.');
+});
